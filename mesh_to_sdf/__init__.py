eaec4fb74f578d53103c29decdbf962919b2fb9d
Gasper Podobnik <gasper.podobnik@gmail.com>
2023-12-27 15:40:59 +0100

    added modifications from DISSM

--- mesh_to_sdf/__init__.py
+++ mesh_to_sdf/__init__.py
@@ -1,7 +1,6 @@
 import numpy as np
 from . import surface_point_cloud
-from .surface_point_cloud import BadMeshException
-from .utils import scale_to_unit_cube, scale_to_unit_sphere, get_raster_points, check_voxels
+from .utils import scale_to_unit_cube, scale_to_unit_sphere
 import trimesh
 
 def get_surface_point_cloud(mesh, surface_point_method='scan', bounding_radius=None, scan_count=100, scan_resolution=400, sample_point_count=10000000, calculate_normals=True):
@@ -49,7 +48,7 @@ def mesh_to_voxels(mesh, voxel_resolution=64, surface_point_method='scan', sign_
     return surface_point_cloud.get_voxels(voxel_resolution, sign_method=='depth', normal_sample_count, pad, check_result, return_gradients)
 
 # Sample some uniform points and some normally distributed around the surface as proposed in the DeepSDF paper
-def sample_sdf_near_surface(mesh, number_of_points = 500000, surface_point_method='scan', sign_method='normal', scan_count=100, scan_resolution=400, sample_point_count=10000000, normal_sample_count=11, min_size=0, return_gradients=False):
+def sample_sdf_near_surface(mesh, number_of_points = 500000, surface_point_method='scan', sign_method='normal', scan_count=100, scan_resolution=400, sample_point_count=10000000, normal_sample_count=11, min_size=0, uniform_proportion=0.5, jitter=0.0025, return_gradients=False):
     mesh = scale_to_unit_sphere(mesh)
     
     if surface_point_method == 'sample' and sign_method == 'depth':
@@ -58,4 +57,4 @@ def sample_sdf_near_surface(mesh, number_of_points = 500000, surface_point_metho
 
     surface_point_cloud = get_surface_point_cloud(mesh, surface_point_method, 1, scan_count, scan_resolution, sample_point_count, calculate_normals=sign_method=='normal' or return_gradients)
 
-    return surface_point_cloud.sample_sdf_near_surface(number_of_points, surface_point_method=='scan', sign_method, normal_sample_count, min_size, return_gradients)
+    return surface_point_cloud.sample_sdf_near_surface(number_of_points, surface_point_method=='scan', sign_method, normal_sample_count, min_size, uniform_proportion=uniform_proportion, jitter=jitter, return_gradients=return_gradients)
